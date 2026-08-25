f30a1832951b87b93d0cd6881e23607a8f3ea78a
Frederic Ruget <fred@atlant.is>
2026-08-25 13:01:51 +0000

    feat(grpconf2026): polish demo script for grpconf talk
    - Add clear before each section header for clean slides
    - Use view/view_textproto helpers instead of nvim -R invocations
    - Switch to protolens --script beats/capture.script for the capture demo
    - Minor wording tweaks (remove wireshark ref, tighten commentary)

--- grpconf2026/grpconf2026.sh
+++ grpconf2026/grpconf2026.sh
@@ -1,6 +1,6 @@
-header "S3NS"
+clear && header "S3NS"
 # \
-# S3NS is Thales x Google joint venture that operates a GCP region as a Trusted Cloud service, for European customers. The service is hosted on French data centers and operated by French personel, in autonomy. However Google provides all software updates, which are inspected and tested in a quarantine environment before being deployed to production. As part of the inspection activities, we have developed tooling to help audit protobufs, which are ubiquitous in the design of the Google Cloud platform.
+# S3NS is Thales x Google joint venture that operates a GCP region as a Trusted Cloud service, for European customers. The service is hosted on French data centers and operated by French personel, in autonomy. However Google provides all software updates, which are inspected and tested in a quarantine environment before being deployed to production. As part of the inspection activities, we have developed tooling to help audit protobufs, which are ubiquitous in the design of the Google Cloud platform.                                                                \
 
 
 header "prototools"
@@ -8,7 +8,7 @@ header "prototools"
 # prototools is 3 CLIs :                                                       \
 #                                                                              \
 # - protoscan: extraction of FileDescriptorProto descriptors from executables  \
-# - reproto: analysis and decompilation of FileDescriptor sets                 \
+# - reproto:   analysis and decompilation of FileDescriptor sets               \
 # - prototext: lossless ser/deser or protobufs, using schemas                  \
 
 # \
@@ -17,12 +17,12 @@ header "prototools"
 # - protolens: interactive TUI for analysing binary protobufs                  \
 
 
-header "The stage"
+clear && header "The stage"
 
 # \
 # 👨 Bob downloaded an unknown executable and an associated log file.          \
 # The executable seems to answer routing questions by calling some external    \
-# service. Bob captured one of its calls with wireshark.                       \
+# service. Bob captured one of its network calls.                              \
 
 
 ls -lh bob \
@@ -34,15 +34,18 @@ ls -lh bob \
 
 # \
 # Bob suspects the calls to be gRPC, and the logs to be protobuf.              \
-# 👩 Alice is handed the lot for analysis 🚀                                   \
 
 
-header "2. protoc falls short"
+# \
+# 👩 Alice is handed the lot for analysis 🚀🚀🚀                               \
+
+
+clear && header "2. protoc falls short"
 
 # Let's try protoc for decoding the capture file:
-protoc --decode_raw < bob/capture \
-  | nvim -R - -c "set ft=textproto"
-# OK. This is protobuf indeed.
+protoc --decode_raw < bob/capture   | view_textproto
+# OK. This is protobuf indeed 🙂
+# But we lack descriptors to make sense of it 🤔...
 
 # Let's try with the logfile:
 protoc --decode_raw < bob/logfile
@@ -50,8 +53,9 @@ protoc --decode_raw < bob/logfile
 
 header "3. Let's retrieve descriptors"
 
-# protoscan is our friend
-protoscan bob/app    | nvim -R -
+# protoscan and reproto are our friends
+
+protoscan bob/app   | view
 # So bob/app contains reflected descriptors indeed
 
 # Let's retrieve, decompile and index the descriptors
@@ -59,45 +63,36 @@ reproto --desc-root bob/app --schema-db-out alice/app.desc
 
 ls -lhd alice/app.desc alice/app/* \
 # A couple of files were produced:                                             \
-# - app.desc: FileDescriptorSet for descriptors in app                         \
-# - app/hopcroft.rkyv: Graph for type inference                                \
-# - app/index.rkyv: Index for fast access and loading                          \
-# - app/proto/: Decompiled descriptors                                         \
+# - app.desc:           Set of descriptors found in app                        \
+# - app/hopcroft.rkyv:  Graph for type inference                               \
+# - app/index.rkyv:     Index for fast access and loading                      \
+# - app/proto/:         Decompiled descriptors                                 \
 
 
-tree alice/app/proto    | nvim -R -
+tree alice/app/proto   | view
 # Let's look at one decompiled .proto file:
-nvim -R alice/app/proto/google/maps/places/v1/places_service.proto
+view alice/app/proto/google/maps/places/v1/places_service.proto
 
 header "4. Let's use the descriptors"
 
-# prototext is our friend 🍀🍀🍀
-\
-prototext --descriptor-set alice/app.desc decode bob/capture \
-  | nvim -R - -c "set ft=textproto"
+# prototext is our friend 🙏
+prototext --descriptor-set alice/app.desc decode bob/capture   | view_textproto
 
-# \
-# prototext was able to infer capture's protobuf type:                         \
-#   google.maps.places.v1.SearchTextRequest                                    \
-# Let's use this with protoc                                                   \
-
+# prototext was able to infer capture's protobuf type 💪💪💪
+#   👉 google.maps.places.v1.SearchTextRequest
+# Let's use this with protoc:
 \
 protoc --descriptor_set_in=alice/app.desc \
        --decode=google.maps.places.v1.SearchTextRequest \
-    < bob/capture \
-  | nvim -R - -c "set ft=textproto"
-# Now protoc is happy 🙂
+    < bob/capture   | view_textproto
+# Now protoc is happy 🍀🍀🍀
 
-# Compare again with the prototext output:
-prototext --descriptor-set alice/app.desc decode bob/capture \
-  | nvim -R - -c "set ft=textproto"
-# \
-# Notice that prototext adds annotations as comments                           \
-# There is more to capture than meets the protoc eye 🕵                         \
+# Compare again with the prototext output.
+# This time, let's use protolens, the interactive version of prototext:
+protolens --descriptor-set alice/app.desc bob/capture   --script beats/capture.script
+# Notice that protolens adds annotations as comments
+# There is more to capture than met the protoc eye 🕵
 
-
-# We can also use protolens, the interactive version of prototext:
-protolens --descriptor-set alice/app.desc bob/capture
 
 # Let's try with the logfile:
 protolens --descriptor-set alice/app.desc bob/logfile
